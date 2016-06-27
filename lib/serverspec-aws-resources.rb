818fee7a97838930aeb0a0fbc0fbc20b0541a713
aheumaier <andreas.heumaier@nordcloud.com>
2016-06-27 11:10:44 +0200

    Added cloudwatch class extention

--- lib/serverspec-aws-resources.rb
+++ lib/serverspec-aws-resources.rb
@@ -15,4 +15,25 @@ require_relative 'resources/auto_scaling_group_resource'
 require_relative 'resources/elb_resource'
 require_relative 'resources/iam_role_resource'
 require_relative 'resources/cloudtrail_resource'
+require_relative 'resources/cloudwatch'
 
+# Check if the initialization argument of an AWS resource class is present
+# @param arg_name [String] - The name of the init argument
+# @param class_name [String] - The name of the AWS resource class
+# @param arg [String] - The arg passed to the class constructor
+# @raise [RuntimeError] if arg.nil?
+def check_init_arg(arg_name, class_name, arg)
+  raise "Must specify #{arg_name} for #{class_name}" if arg.nil?
+end
+
+# Check the length for operations that should return only one resource
+# @param item_name [String] - The name of the item to check
+# @param item [Array] - The actual item for checking the length
+# @raise [RuntimeError] if item.length == 0
+# @raise [RuntimeError] if item.length > 1
+def check_length(item_name, item)
+  raise "No #{item_name} with the specified name were "\
+             'returned' if item.empty?
+  raise "Multiple #{item_name} with the same name "\
+             'were returned' if item.length > 1
+end
